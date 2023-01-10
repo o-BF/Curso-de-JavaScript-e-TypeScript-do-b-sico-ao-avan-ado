befaf016320938c35e74aab711a5648f0a940ded
FelipeGoncalves <felipegoncalves@pop-os.localdomain>
2023-01-10 20:04:40 -0300

    day10

---
+++ curso js/seção 1/arrays.js
@@ -0,0 +1,41 @@
+const alunos = ['Luiz' , 'Felipe' , 'Joao'] // arrays são tudo dentro de um colchete
+/* Pegar elementos do array = console.log(alunos[indice]); */
+
+
+/* tambem adciona elementos no fim do array
+alunos[alunos.length] = 'Luiza'
+alunos[alunos.length] = 'Flavinho'
+alunos[alunos.length] = 'Gustavo'
+*/
+
+/* tambem adciona elementos no fim do array
+alunos.push('Luiza')
+alunos.push('Joaquim')
+*/
+
+/* adciona elementos no inicio do array
+alunos.unshift('Luiza')
+alunos.unshift('Joaquim')
+*/
+
+/* remove elementos no final do array, podem ser guardados em variaveis
+alunos.pop();
+*/
+
+/* remove elementos no inicio do array, podem...
+alunos.shift();
+*/
+
+/* remove um elemento especefico, e o espaço fica vazio
+delete alunos[1];
+*/
+
+
+
+
+/* alunos[0] = 'Marcos';  // altera o valor de um elemento selecionado
+alunos[3] = 'Luiza'; // criando um elemento dentro do array
+*/
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
